a57842559e427da4240ad1f5c24e143271640aec
Reza Goodarzi <rezagoodarzi026@gmail.com>
2021-05-30 21:42:24 +0430

    Service section

---
+++ src/components/Service/Service.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+import service1 from '../../assets/images/service/service1.png';
+import service2 from '../../assets/images/service/service2.png';
+import service3 from '../../assets/images/service/service3.png';
+
+const Service = () => {
+	return (
+		<Container>
+			<Image src={service1} alt="Service 1" />
+			<Image src={service2} alt="Service 2" />
+			<Image src={service3} alt="Service 3" />
+		</Container>
+	);
+};
+
+export default Service;
+
+const Container = styled.section`
+	padding: 5rem;
+	display: flex;
+	justify-content: center;
+	overflow-x: scroll;
+`;
+
+const Image = styled.img`
+	width: 32%;
+	margin: 1rem;
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Basket from "../components/Basket/Basket";
 import Search from "../components/Search/Search";
+import Service from "../components/Service/Service";
 
 const Home = () => {
 	return (
 		<>
 			<Search />
 			<Basket />
+			<Service />
 		</>
 	);
 };
