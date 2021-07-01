b3b31224858ab45a07872f94061efba522334315
Reza Goodarzi <rezagoodarzi026@gmail.com>
2021-07-01 23:41:18 +0430

    Biuld YourBill component

--- src/components/YourOrder/YourBill.js
+++ src/components/YourOrder/YourBill.js
@@ -1,9 +1,49 @@
+import styled from "styled-components";
+
 function YourBill() {
 	return (
-		<div>
-			Bill
-		</div>
+		<YourBillStyle>
+			<span className="sub_total">
+				<p className="title">جمع ریز</p>
+				<p className="price">720,000 ت</p>
+			</span>
+			<span className="delivery_fee">
+				<p className="title">هزینه ارسال</p>
+				<p className="price">0 ت</p>
+			</span>
+			<span className="discount">
+				<p className="title">تخفیف</p>
+				<p className="price">72,000 ت</p>
+			</span>
+			<span className="total">
+				<p className="title">جمع کل</p>
+				<p className="price">648,000 ت</p>
+			</span>
+		</YourBillStyle>
 	);
 }
 
 export default YourBill;
+
+const YourBillStyle = styled.div`
+
+	span{
+		display: flex;
+		justify-content: space-between;
+		color: var(--color-gray);
+
+		p{
+			margin: 7px 3px;
+		}
+	}
+
+	.total{
+		color: var(--color-black);
+		margin-top: 1rem;
+
+		p{
+			font-weight: 500;
+		}
+	}
+
+`;
