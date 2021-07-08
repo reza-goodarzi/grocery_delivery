2637000dba1471487ef66934af50ac78e858f0c3
Reza Goodarzi <rezagoodarzi026@gmail.com>
2021-07-08 16:46:12 +0430

    show item of user add to cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,25 +1,27 @@
-
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import tempImg from '../../assets/images/Items/item1.jpg';
+import { addItemToCart, removeItemFromCart } from '../../store/cartSlice';
 import { CircleButton, FontIcon } from '../../styles/style';
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
+	const dispatch = useDispatch();
+
 	return (
 		<Item>
 			<Numbers>
-				<FontIconStyle icon={faPlus} />
-				<span className="number">5</span>
-				<FontIconStyle icon={faMinus} />
+				<FontIconStyle icon={faPlus} onClick={() => dispatch(addItemToCart(item))} />
+				<span className="number">{item.quantity}</span>
+				<FontIconStyle icon={faMinus} onClick={() => dispatch(removeItemFromCart(item.id))} />
 			</Numbers>
 			<Image>
-				<img src={tempImg} alt="item" />
+				<img src={item.image} alt={item.name} />
 			</Image>
 			<Details>
-				<span className="product-name">اسم محصولم حصولم حصول محصولم محصولم	</span>
-				<span className="price">25000 تومان</span>
+				<span className="product-name">{item.name}</span>
+				<span className="price">{`${item.price * 1000} تومان`}</span>
 			</Details>
-			<TotalPrice>50,000 تومان</TotalPrice>
+			<TotalPrice>{`${item.totalPrice * 1000} تومان`}</TotalPrice>
 			<CircleButton>X</CircleButton>
 		</Item>
 	);
@@ -31,12 +33,12 @@ const Item = styled.div`
 	color: var(--color-black);
 	font-size: 1.4rem;
 	border-bottom: 1px solid #eee;
-	height: 12rem;
+	height: 10rem;
 	padding: 1rem 3rem;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-`;
+	`;
 
 const Numbers = styled.div`
 	background-color: #eee;
@@ -52,7 +54,6 @@ const Numbers = styled.div`
 		/* margin: auto; */
 		font-weight: bold;
 	}
-
 `;
 
 const FontIconStyle = styled(FontIcon)`
@@ -62,7 +63,7 @@ const FontIconStyle = styled(FontIcon)`
 `;
 
 const Image = styled.div`
-	width: 8rem;
+	width: 10rem;
 	display: flex;
 	justify-content: center;
 
@@ -77,18 +78,19 @@ const Details = styled.div`
 	align-items: flex-start;
 	justify-content: space-around;
 	height: 100%;
-	width: 15rem;
+	width: 13rem;
 	margin: 0 1rem;
 	font-size: 1.3rem;
 
 	.product-name{
 		font-weight: bold;
+		font-size: 1.6rem;
 	}
 
 	.price{
-		color: var(--color-primary)
+		color: var(--color-primary);
+		font-size: 1.4rem;
 	}
-	
 `;
 
 const TotalPrice = styled.span`
